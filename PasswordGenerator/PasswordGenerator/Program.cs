0a5b8a173198cedfd1ca8e79cc48504b4c9ab615
Chinzcode <132280616+Chinzcode@users.noreply.github.com>
2023-11-14 22:59:50 +0100

    made changes

--- PasswordGenerator/PasswordGenerator/Program.cs
+++ PasswordGenerator/PasswordGenerator/Program.cs
@@ -17,6 +17,23 @@
                 Console.WriteLine("PasswordGenerator  \r\nOptions:\r\n- l = liten bokstav\r\n- L = stor bokstav\r\n- d = siffer\r\n- s = spesialtegn (!\"#\u00a4%&/(){}[]\r\nEksempel: PasswordGenerator 14 lLssdd\r\n    betyr\r\n        Min. 1 liten bokstav\r\n        Min. 1 1 stor bokstav\r\n        Min. 2 spesialtegn\r\n        Min. 2 sifre\r\n        Lengde på passordet skal være 14");
             }
 
+            GenerateRandomPattern(args);
+        }
+        static bool IsValid(string[] args)
+        {
+            var charOptions = "Llds";
+
+            if (args.Length != 2) return false;
+
+            foreach (var c1 in args[0]) if (!char.IsDigit(c1)) return false;
+
+            foreach (var c2 in args[1]) if (!charOptions.Contains(c2)) return false;
+
+            return true;
+        }
+
+        private static void GenerateRandomPattern(string[] args)
+        {
             var pattern = args[1];
             var lengthArg = Convert.ToInt32(args[0]);
 
@@ -25,7 +42,7 @@
                 pattern += 'l';
             }
 
-            while (pattern.Length > 1)
+            while (pattern.Length > 0)
             {
                 var randomNumb = Random.Next(0, pattern.Length);
                 var removedChar = pattern[randomNumb];
@@ -49,27 +66,14 @@
 
             void WriteRandomDigit()
             {
-                Console.WriteLine(Random.Next(1,20));
+                Console.WriteLine(Random.Next(1, 20));
             }
 
             void WriteRandomSpecialCharacter()
             {
                 var specialChars = "!#&()=?{}[]";
-                Console.WriteLine(Random.Next(0, 10));
+                Console.WriteLine(specialChars[Random.Next(0, 10)]);
             }
-        }
-
-        static bool IsValid(string[] args)
-        {
-            var charOptions = "Llds";
-
-            if (args.Length != 2) return false;
-
-            foreach (var c1 in args[0]) if (!char.IsDigit(c1)) return false;
-
-            foreach (var c2 in args[1]) if (!charOptions.Contains(c2)) return false;
-
-            return true;
         }
 
         static char GetRandomLetter(char min, char max)
