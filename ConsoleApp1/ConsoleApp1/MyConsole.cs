26ab62847d7c3f3933fbdbb3e96d767293ad8313
Chinzcode <132280616+Chinzcode@users.noreply.github.com>
2023-11-15 13:25:11 +0100

    added new class and finished code

---
+++ ConsoleApp1/ConsoleApp1/MyConsole.cs
@@ -0,0 +1,23 @@
+﻿namespace ConsoleApp1
+{
+    internal class MyConsole
+    {
+        public static string Ask(string question)
+        {
+            Console.Write(question + " ");
+            return Console.ReadLine();
+        }
+
+        public static int AskForInt(string question)
+        {
+            var answerStr = Ask(question);
+            return Convert.ToInt32(answerStr);
+        }
+
+        public static bool AskForBool(string question)
+        {
+            var answerStr = Ask( question + " (Yes/No):");
+            return answerStr.ToLower().StartsWith("y");
+        }
+    }
+}
--- ConsoleApp1/ConsoleApp1/Program.cs
+++ ConsoleApp1/ConsoleApp1/Program.cs
@@ -4,15 +4,11 @@
     {
         static void Main(string[] args)
         {
-            Console.Write("What is your name? ");
-            var name = Console.ReadLine();
-            Console.Write($"Hi {name}, what year are you born? ");
-            var birthyearStr = Console.ReadLine();
-            var birthyear = Convert.ToInt32(birthyearStr);
+            var name = MyConsole.Ask("What is your name?");
+            var birthyear = MyConsole.AskForInt($"Hi {name}, what year are you born?");
+            var hadBirthday = MyConsole.AskForBool("Have you had a birthday this year?");
             var age = 2023 - birthyear;
-            Console.Write("Have you had a birthday this year? (Yes/No): ");
-            var answer = Console.ReadLine();
-            if (answer == "No") age--; 
+            if (!hadBirthday) age--;
             Console.WriteLine($"You are {age} years old");
         }
     }
