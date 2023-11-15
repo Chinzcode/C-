5a3c3975dc39894467bd7d4cedbdd02dad2dd866
Chinzcode <132280616+Chinzcode@users.noreply.github.com>
2023-11-15 13:04:49 +0100

    first commit for ConsoleApp1

---
+++ ConsoleApp1/ConsoleApp1/Program.cs
@@ -0,0 +1,19 @@
+﻿namespace ConsoleApp1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("What is your name? ");
+            var name = Console.ReadLine();
+            Console.Write($"Hi {name}, what year are you born? ");
+            var birthyearStr = Console.ReadLine();
+            var birthyear = Convert.ToInt32(birthyearStr);
+            var age = 2023 - birthyear;
+            Console.Write("Have you had a birthday this year? (Yes/No): ");
+            var answer = Console.ReadLine();
+            if (answer == "No") age--; 
+            Console.WriteLine($"You are {age} years old");
+        }
+    }
+}
