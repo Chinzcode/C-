c72e45b45fe4488a3f889fea7d1e9cd7b5d93a7b
Chinzcode <132280616+Chinzcode@users.noreply.github.com>
2023-11-17 15:03:30 +0100

    made changes

--- WordPuzzle/WordPuzzle/Program.cs
+++ WordPuzzle/WordPuzzle/Program.cs
@@ -8,28 +8,34 @@
         static void Main(string[] args)
         {
             string[] strArray = ReturnArray.GetArrayOfString(7, 10);
-            var randomWord = GetRandomWord(strArray);
+            //var randomWord = GetRandomWord(strArray).ToLower();
+            var randomWord = "abonnere";
 
             var lastThreeChars = getLastCharsInString(randomWord, 3);
             var lastFourChars = getLastCharsInString(randomWord, 4);
-            var lastFiveChars = getLastCharsInString(randomWord, 4);
+            var lastFiveChars = getLastCharsInString(randomWord, 5);
 
             Console.WriteLine("randomWord = " + randomWord);
-            Console.WriteLine("lastThreeChars = " + lastThreeChars);
 
             foreach (var element in strArray)
             {
                 if (string.Equals(element.Substring(0, 3), lastThreeChars))
                 {
                     Console.WriteLine("element = " + element);
-                } else if (string.Equals(element.Substring(0, 4), lastFourChars))
+                    Console.WriteLine("first 3 match");
+                }
+                else if (string.Equals(element.Substring(0, 4), lastFourChars))
                 {
                     Console.WriteLine("element = " + element);
-                } else if (string.Equals(element.Substring(0, 5), lastFiveChars))
+                    Console.WriteLine("first 4 match");
+                }
+                else if (string.Equals(element.Substring(0, 5), lastFiveChars))
                 {
                     Console.WriteLine("element = " + element);
+                    Console.WriteLine("first 5 match");
                 }
             }
+
         }
 
         static string getLastCharsInString(string randomWord, int length)
@@ -40,7 +46,7 @@
         static string GetRandomWord(string[] strArray)
         {
             var random = new Random();
-            var randomNum = random.Next(1, strArray.Length);
+            var randomNum = random.Next(1, strArray.Length-1);
             return strArray[randomNum];
         }
     }
