04ad19a3bc3a12bec53b7ad93fb23d191b1afe93
Chinzcode <132280616+Chinzcode@users.noreply.github.com>
2023-11-17 09:57:55 +0100

    first commit

---
+++ WordPuzzle/WordPuzzle/Program.cs
@@ -0,0 +1,28 @@
+﻿namespace WordPuzzle
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            var filename = "Dictionary.txt";
+            string[] textArray = File.ReadAllLines(filename);
+            string lastWord = null;
+
+            for (var i = 0; i < textArray.Length/10; i++)
+            {
+                string[] parts = textArray[i].Split('\t');
+
+                if (parts.Length > 0)
+                {
+                    var word = parts[1];
+
+                    if (word != lastWord)
+                    {
+                        Console.WriteLine(word);
+                        lastWord = word;
+                    }
+                }
+            }
+        }
+    }
+}
