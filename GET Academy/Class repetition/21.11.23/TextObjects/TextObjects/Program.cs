ec0539d4e0315f438a086ec977d1a0b476c7b053
Chinzcode <132280616+Chinzcode@users.noreply.github.com>
2023-11-21 13:09:19 +0100

    first commit

---
+++ GET Academy/Class repetition/21.11.23/TextObjects/TextObjects/Program.cs
@@ -0,0 +1,16 @@
+﻿using TextObjects;
+
+/*
+Declare new object
+Make new class and assign public and type
+Use enum to make case options
+*/
+var myText = new TextEffects
+{
+    Text = "Hallo",
+    Color = ConsoleColor.Red,
+    Col = 3,
+    Rows = 10,
+    IsInverted = false,
+    Case = Case.Default,
+};
---
+++ GET Academy/Class repetition/21.11.23/TextObjects/TextObjects/TextEffects.cs
@@ -0,0 +1,12 @@
+﻿namespace TextObjects
+{
+    internal class TextEffects
+    {
+        public string Text;
+        public ConsoleColor Color;
+        public int Col;
+        public int Rows;
+        public bool IsInverted;
+        public Case Case;
+    }
+}
