240ce9ff017caca4e0e19496eeee1df9dbd8b70c
Chinzcode <132280616+Chinzcode@users.noreply.github.com>
2023-11-21 13:57:41 +0100

    added comments

---
+++ GET Academy/Class repetition/21.11.23/TextObjects/TextObjects/Comments.cs
@@ -0,0 +1,87 @@
+﻿/*
+Declare new object
+Make new class and assign public and type
+Use enum to make case options
+*/
+
+//-------------------------------------------------------------//
+
+//var myText = new TextEffect();
+//myText.Text = "AAAAA";
+
+/*
+var myText1 = new TextEffects()
+{
+    Text = "AAAAA",
+    Color = ConsoleColor.Red,
+    Col = 1,
+    Rows = 1,
+    IsInverted = false,
+    Case = Case.Default,
+}; 
+*/
+/*
+var myText2 = new TextEffects()
+{
+    Text = "BBB",
+    Color = ConsoleColor.Blue,
+    Col = 2,
+    Rows = 2,
+    IsInverted = false,
+    Case = Case.Upper,
+}; 
+*/
+/*
+var myText3 = new TextEffects()
+{
+    Text = "C",
+    Color = ConsoleColor.Green,
+    Col = 3,
+    Rows = 3,
+    IsInverted = true,
+    Case = Case.Lower,
+}; 
+*/
+
+//-------------------------------------------------------------//
+
+//var myTexts = new TextEffect[3];
+//myTexts[0] = myText1;
+
+/*
+var myTexts = new TextEffects[] { myText1, myText2, myText3 };
+Console.WriteLine(myTexts[0].Text); = AAAAA
+Console.WriteLine(myTexts[1].Text); = BBB
+Console.WriteLine(myTexts[2].Text); = C
+*/
+
+//-------------------------------------------------------------//
+
+/*
+var myTexts = new TextEffects[] {
+    new TextEffects() {
+        Text = "AAAAA",
+        Color = ConsoleColor.Red,
+        Col = 1,
+        Rows = 1,
+        IsInverted = false,
+        Case = Case.Default,
+    },
+    new TextEffects() {
+        Text = "BBB",
+        Color = ConsoleColor.Blue,
+        Col = 2,
+        Rows = 2,
+        IsInverted = false,
+        Case = Case.Upper,
+    },
+    new TextEffects() {
+        Text = "C",
+        Color = ConsoleColor.Green,
+        Col = 3,
+        Rows = 3,
+        IsInverted = true,
+        Case = Case.Lower,
+    },
+};
+*/
