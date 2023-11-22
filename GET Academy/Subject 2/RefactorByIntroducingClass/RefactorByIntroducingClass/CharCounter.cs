0f9faa3adac57c9acb1e2c8c57702f31110ea5f2
Chinzcode <132280616+Chinzcode@users.noreply.github.com>
2023-11-22 13:48:51 +0100

    refactored by adding class

---
+++ GET Academy/Subject 2/RefactorByIntroducingClass/RefactorByIntroducingClass/CharCounter.cs
@@ -0,0 +1,33 @@
+﻿namespace RefactorByIntroducingClass
+{
+    internal class CharCounter
+    {
+        private int[] counts;
+
+        public CharCounter()
+        {
+            var range = 250;
+            counts = new int[range];
+        }
+
+        public void AddText(string text)
+        {
+            foreach (var character in text ?? string.Empty)
+            {
+                counts[(int)character]++;
+            }
+        }
+
+        public void ShowCounts()
+        {
+            for (var i = 0; i < counts.Length; i++)
+            {
+                if (counts[i] > 0)
+                {
+                    var character = (char)i;
+                    Console.WriteLine(character + " - " + counts[i]);
+                }
+            }
+        }
+    }
+}
--- GET Academy/Subject 2/RefactorByIntroducingClass/RefactorByIntroducingClass/Program.cs
+++ GET Academy/Subject 2/RefactorByIntroducingClass/RefactorByIntroducingClass/Program.cs
@@ -4,24 +4,14 @@
     {
         static void Main(string[] args)
         {
-            var range = 250;
-            var counts = new int[range];
+            var counter = new CharCounter();
             string text = "something";
+
             while (!string.IsNullOrWhiteSpace(text))
             {
                 text = Console.ReadLine();
-                foreach (var character in text ?? string.Empty)
-                {
-                    counts[(int)character]++;
-                }
-                for (var i = 0; i < range; i++)
-                {
-                    if (counts[i] > 0)
-                    {
-                        var character = (char)i;
-                        Console.WriteLine(character + " - " + counts[i]);
-                    }
-                }
+                counter.AddText(text);
+                counter.ShowCounts();
             }
         }
     }
