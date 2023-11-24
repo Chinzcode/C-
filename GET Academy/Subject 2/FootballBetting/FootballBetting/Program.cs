06725bff6946bca5c808b57d072d25c4d9b88237
Chinzcode <132280616+Chinzcode@users.noreply.github.com>
2023-11-24 17:08:50 +0100

    refactored and made OOP

--- GET Academy/Subject 2/FootballBetting/FootballBetting/Program.cs
+++ GET Academy/Subject 2/FootballBetting/FootballBetting/Program.cs
@@ -1,33 +1,34 @@
-﻿Console.Write("Gyldig tips: \n" +
-              " - H, U, B\n" +
-              " - halvgardering: HU, HB, UB\n" +
-              " - helgardering: HUB\n" +
-              "Hva har du tippet for denne kampen? ");
+﻿using FootballBetting;
 
-var bet = Console.ReadLine();
-var homeGoals = 0;
-var awayGoals = 0;
-var matchIsRunning = true;
+ShowTips();
+var bet = Console.ReadLine() ?? string.Empty;
+var match = new Match(bet);
+while (match.IsOngoing)
+{
+    ShowCommands();
+    var command = Console.ReadLine();
+    if (command == "X") match.Stop();
+    else if (command == "H" || command == "B") match.AddGoal(command == "H");
+    Console.WriteLine($"Stillingen er {match.GetScore()}.");
+}
 
-while (matchIsRunning)
+var isBetCorrectText = match.IsBetCorrect() ? "riktig" : "feil";
+Console.WriteLine($"Du tippet {isBetCorrectText}");
+
+void ShowTips()
+{
+    Console.Write("Gyldig tips: \n" +
+                  " - H, U, B\n" +
+                  " - halvgardering: HU, HB, UB\n" +
+                  " - helgardering: HUB\n" +
+                  "Hva har du tippet for denne kampen? ");
+}
+
+void ShowCommands()
 {
     Console.Write("Kommandoer: \n" +
                   " - H = scoring hjemmelag\n" +
                   " - B = scoring bortelag\n" +
                   " - X = kampen er ferdig\n" +
                   "Angi kommando: ");
-
-    var command = Console.ReadLine();
-    matchIsRunning = command != "X";
-
-    if (command == "H") homeGoals++;
-    else if (command == "B") awayGoals++;
-
-    Console.WriteLine($"Stillingen er {homeGoals}-{awayGoals}.");
 }
-
-var result = homeGoals == awayGoals ? "U" : homeGoals > awayGoals ? "H" : "B";
-var isBetCorrect = bet.Contains(result);
-var isBetCorrectText = isBetCorrect ? "riktig" : "feil";
-
-Console.WriteLine($"Du tippet {isBetCorrectText}");
