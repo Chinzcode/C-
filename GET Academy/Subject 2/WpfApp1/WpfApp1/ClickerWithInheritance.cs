ecf52a68d73dc1a8fd1acefa0fbe12ab35aa690f
Chinzcode <132280616+Chinzcode@users.noreply.github.com>
2023-11-30 16:18:37 +0100

    feat: implement ClickerWithInheritance class for reusable button functionality
    - Create ClickerWithInheritance class, inheriting from StackPanel
    - Update Program.cs to use ClickerWithInheritance for UI elements
    - Create instances of ClickerWithInheritance for multiple buttons

---
+++ GET Academy/Subject 2/WpfApp1/WpfApp1/ClickerWithInheritance.cs
@@ -0,0 +1,33 @@
+﻿using System.Windows.Controls;
+using System.Windows;
+
+namespace WpfApp1
+{
+    internal class ClickerWithInheritance : StackPanel
+    {
+        private readonly Label Label;
+        private int Count;
+
+        public ClickerWithInheritance() : base()
+        {
+            this.Orientation = Orientation.Horizontal;
+            var button = new Button
+            {
+                Content = "Trykk her!"
+            };
+            button.Click += ButtonClick;
+            this.Label = new Label
+            {
+                Content = "Trykk på knappen!"
+            }; 
+            this.Children.Add(button);
+            this.Children.Add(this.Label);
+        }
+
+        private void ButtonClick(object sender, RoutedEventArgs e)
+        {
+            this.Count++;
+            this.Label.Content = this.Count;
+        }
+    }
+}
--- GET Academy/Subject 2/WpfApp1/WpfApp1/Program.cs
+++ GET Academy/Subject 2/WpfApp1/WpfApp1/Program.cs
@@ -2,63 +2,97 @@
 using System.Windows;
 using System.Windows.Controls;
 
-//Version 1
+//Version 3
 namespace WpfApp1
 {
     internal class Program
     {
-        private static int _count;
-        private static Label _label;
         [STAThread]
         public static void Main(string[] args)
         {
             var app = new Application();
             var window = new Window();
-            var clicker = new Clicker();
-            window.Content = clicker.Panel;
+            var clicker1 = new ClickerWithInheritance();
+            var clicker2 = new ClickerWithInheritance();
+            var clicker3 = new ClickerWithInheritance();
+
+            var panel = new StackPanel();
+            panel.Children.Add(clicker1);
+            panel.Children.Add(clicker2);
+            panel.Children.Add(clicker3);
+            window.Content = panel;
             app.Run(window);
         }
     }
 }
 
+/* Version 2 with class
+
+namespace WpfApp1
+{
+   internal class Program
+   {
+        private static int _count;
+        private static Label _label;
+        [STAThread]
+
+        public static void Main(string[] args)
+        {
+            var app = new Application();
+            var window = new Window();
+            var clicker1 = new Clicker();
+            var clicker2 = new Clicker();
+            var clicker3 = new Clicker();
+
+            var panel = new StackPanel();
+            panel.Children.Add(clicker1.Panel);
+            panel.Children.Add(clicker2.Panel);
+            panel.Children.Add(clicker3.Panel);
+            window.Content = panel;
+            app.Run(window);
+        }
+   }
+}
+ */
+
 /* Version 1
 
-   namespace WpfApp1
+namespace WpfApp1
+{
+   internal class Program
    {
-       internal class Program
-       {
-           private static int _count;
-           private static Label _label;
-           [STAThread]
+       private static int _count;
+       private static Label _label;
+       [STAThread]
 
-           public static void Main(string[] args)
+       public static void Main(string[] args)
+       {
+           var app = new Application();
+           var window = new Window();
+           var panel = new StackPanel
            {
-               var app = new Application();
-               var window = new Window();
-               var panel = new StackPanel
-               {
-                    Orientation = Orientation.Horizontal
-               };
-               var button = new Button
-               {
-                    Content = "Trykk her!"
-               };
-               button.Click += ButtonClick;
-               _label = new Label
-               {
-                    Content = "Trykk på knappen!"
-               };
-               panel.Children.Add(button);
-               panel.Children.Add(_label);
-               window.Content = panel;
-               app.Run(window);
-            }
-           
-           private static void ButtonClick(object sender, RoutedEventArgs e)
+                Orientation = Orientation.Horizontal
+           };
+           var button = new Button
            {
-               _count++;
-               _label.Content = _count;
-           }
+                Content = "Trykk her!"
+           };
+           button.Click += ButtonClick;
+           _label = new Label
+           {
+                Content = "Trykk på knappen!"
+           };
+           panel.Children.Add(button);
+           panel.Children.Add(_label);
+           window.Content = panel;
+           app.Run(window);
+        }
+       
+       private static void ButtonClick(object sender, RoutedEventArgs e)
+       {
+           _count++;
+           _label.Content = _count;
        }
    }
+}
  */
