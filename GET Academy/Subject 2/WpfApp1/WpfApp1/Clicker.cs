fa6f637f165877b20522758f34cb735aa6883505
Chinzcode <132280616+Chinzcode@users.noreply.github.com>
2023-11-30 15:38:08 +0100

    feat: implement Clicker class for button functionality
    - Create Clicker class to encapsulate button click logic
    - Update Program.cs to use Clicker class for UI elements

---
+++ GET Academy/Subject 2/WpfApp1/WpfApp1/Clicker.cs
@@ -0,0 +1,37 @@
+﻿using System.Windows;
+using System.Windows.Controls;
+
+namespace WpfApp1
+{
+    internal class Clicker
+    {
+        private readonly Label Label;
+        private int Count;
+        public StackPanel Panel { get; }
+
+        public Clicker()
+        {
+            Panel = new StackPanel
+            {
+                Orientation = Orientation.Horizontal
+            };
+            var button = new Button
+            {
+                Content = "Trykk her!"
+            };
+            button.Click += ButtonClick;
+            this.Label = new Label
+            {
+                Content = "Trykk på knappen!"
+            };
+            Panel.Children.Add(button);
+            Panel.Children.Add(this.Label);
+        }
+
+        private void ButtonClick(object sender, RoutedEventArgs e)
+        {
+            this.Count++;
+            this.Label.Content = this.Count;
+        }
+    }
+}
--- GET Academy/Subject 2/WpfApp1/WpfApp1/Program.cs
+++ GET Academy/Subject 2/WpfApp1/WpfApp1/Program.cs
@@ -14,29 +14,51 @@ namespace WpfApp1
         {
             var app = new Application();
             var window = new Window();
-            var panel = new StackPanel
-            {
-                Orientation = Orientation.Horizontal
-            };
-            var button = new Button
-            {
-                Content = "Trykk her!"
-            };
-            button.Click += ButtonClick;
-            _label = new Label
-            {
-                Content = "Trykk på knappen!"
-            };
-            panel.Children.Add(button);
-            panel.Children.Add(_label);
-            window.Content = panel;
+            var clicker = new Clicker();
+            window.Content = clicker.Panel;
             app.Run(window);
-        }
-
-        private static void ButtonClick(object sender, RoutedEventArgs e)
-        {
-            _count++;
-            _label.Content = _count;
         }
     }
 }
+
+/* Version 1
+
+   namespace WpfApp1
+   {
+       internal class Program
+       {
+           private static int _count;
+           private static Label _label;
+           [STAThread]
+
+           public static void Main(string[] args)
+           {
+               var app = new Application();
+               var window = new Window();
+               var panel = new StackPanel
+               {
+                    Orientation = Orientation.Horizontal
+               };
+               var button = new Button
+               {
+                    Content = "Trykk her!"
+               };
+               button.Click += ButtonClick;
+               _label = new Label
+               {
+                    Content = "Trykk på knappen!"
+               };
+               panel.Children.Add(button);
+               panel.Children.Add(_label);
+               window.Content = panel;
+               app.Run(window);
+            }
+           
+           private static void ButtonClick(object sender, RoutedEventArgs e)
+           {
+               _count++;
+               _label.Content = _count;
+           }
+       }
+   }
+ */
