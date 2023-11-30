ca5096e37e11fe7f4f8b4c6c4c611c33f33bbe45
Chinzcode <132280616+Chinzcode@users.noreply.github.com>
2023-11-30 15:28:17 +0100

    feat: add button click functionality to update label

--- GET Academy/Subject 2/WpfApp1/WpfApp1/Program.cs
+++ GET Academy/Subject 2/WpfApp1/WpfApp1/Program.cs
@@ -2,10 +2,13 @@
 using System.Windows;
 using System.Windows.Controls;
 
+//Version 1
 namespace WpfApp1
 {
     internal class Program
     {
+        private static int _count;
+        private static Label _label;
         [STAThread]
         public static void Main(string[] args)
         {
@@ -19,14 +22,21 @@ namespace WpfApp1
             {
                 Content = "Trykk her!"
             };
-            var label = new Label
+            button.Click += ButtonClick;
+            _label = new Label
             {
                 Content = "Trykk på knappen!"
             };
             panel.Children.Add(button);
-            panel.Children.Add(label);
+            panel.Children.Add(_label);
             window.Content = panel;
             app.Run(window);
+        }
+
+        private static void ButtonClick(object sender, RoutedEventArgs e)
+        {
+            _count++;
+            _label.Content = _count;
         }
     }
 }
