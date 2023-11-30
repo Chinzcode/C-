e7d30069175e775a377bd8a34469f780c49c321f
Chinzcode <132280616+Chinzcode@users.noreply.github.com>
2023-11-30 10:42:26 +0100

    first commit

---
+++ GET Academy/Subject 2/Players/Players/Player.cs
@@ -0,0 +1,28 @@
+﻿namespace Players
+{
+    internal class Player
+    {
+        private string Name;
+        private int Points;
+
+        public Player(string name, int points)
+        {
+            this.Name = name;
+            this.Points = points;
+        }
+
+        public void Play(Player player2)
+        {
+            var random = new Random();
+            var winner = random.Next(2) == 0 ? this : player2;
+            var loser = winner == this ? player2 : this;
+            winner.Points++;
+            loser.Points--;
+        }
+
+        public void ShowNameAndPoints()
+        {
+            Console.WriteLine($"Name: {this.Name}, points: {this.Points}");
+        }
+    }
+}
