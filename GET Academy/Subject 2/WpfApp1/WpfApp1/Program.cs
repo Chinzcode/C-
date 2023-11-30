5c1e75eb7347536408dbd3a88fff2dba67c45abb
Chinzcode <132280616+Chinzcode@users.noreply.github.com>
2023-11-30 14:55:13 +0100

    feat: implement basic WPF application

--- GET Academy/Subject 2/WpfApp1/WpfApp1/Program.cs
+++ GET Academy/Subject 2/WpfApp1/WpfApp1/Program.cs
@@ -1,6 +1,32 @@
-﻿namespace WpfApp1
+﻿using System;
+using System.Windows;
+using System.Windows.Controls;
+
+namespace WpfApp1
 {
     internal class Program
     {
+        [STAThread]
+        public static void Main(string[] args)
+        {
+            var app = new Application();
+            var window = new Window();
+            var panel = new StackPanel
+            {
+                Orientation = Orientation.Horizontal
+            };
+            var button = new Button
+            {
+                Content = "Trykk her!"
+            };
+            var label = new Label
+            {
+                Content = "Trykk på knappen!"
+            };
+            panel.Children.Add(button);
+            panel.Children.Add(label);
+            window.Content = panel;
+            app.Run(window);
+        }
     }
 }
