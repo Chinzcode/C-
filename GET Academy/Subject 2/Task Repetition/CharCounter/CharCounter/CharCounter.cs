d70b1f9dc830bd5c07a533045cdcb99cd694cd05
Chinzcode <132280616+Chinzcode@users.noreply.github.com>
2023-12-06 10:46:45 +0100

    feat: refactor code using CharCounter class
    - Replace the original code with a new implementation using the CharCounter class
    - Introduce CharCounter class to handle character counting logic

---
+++ GET Academy/Subject 2/Task Repetition/CharCounter/CharCounter/CharCounter.cs
@@ -0,0 +1,33 @@
+﻿namespace CharCounterTask
+{
+    internal class CharCounter
+    {
+        private int[] Counts;
+        private int Range;
+
+        public CharCounter()
+        {
+            this.Range = 250;
+            this.Counts = new int[Range];
+        }
+
+        public void AddText(string text)
+        {
+            foreach (var character in text ?? string.Empty)
+            {
+                this.Counts[(int)character]++;
+            }
+        }
+        public void ShowCounts()
+        {
+            for (var i = 0; i < this.Range; i++)
+            {
+                if (this.Counts[i] > 0)
+                {
+                    var character = (char)i;
+                    Console.WriteLine(character + " - " + this.Counts[i]);
+                }
+            }
+        }
+    }
+}
---
+++ GET Academy/Subject 2/Task Repetition/CharCounter/CharCounter/Program.cs
@@ -0,0 +1,10 @@
+﻿using CharCounterTask;
+
+var counter = new CharCounter();
+string text = "something";
+while (!string.IsNullOrWhiteSpace(text))
+{
+    text = Console.ReadLine() ?? string.Empty;
+    counter.AddText(text);
+    counter.ShowCounts();
+}
