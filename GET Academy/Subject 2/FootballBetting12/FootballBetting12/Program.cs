a673710defb08779613624c68c4e206e7c3bbe1c
Chinzcode <132280616+Chinzcode@users.noreply.github.com>
2023-12-06 14:43:15 +0100

    chore: initialize project

---
+++ GET Academy/Subject 2/FootballBetting12/FootballBetting12/Program.cs
@@ -0,0 +1,34 @@
+﻿using FootballBetting12;
+
+WriteTips();
+var bet = Console.ReadLine();
+var match = new Match();
+
+while (match.IsMatchRunning())
+{
+    WriteCommand();
+    var command = Console.ReadLine();
+    match.CheckCommands(command ?? string.Empty);
+    Console.WriteLine($"Stillingen er {match.GetCurrentScore()}.");
+}
+
+var isBetCorrectText = match.IsBetCorrect(bet ?? string.Empty) ? "riktig" : "feil";
+Console.WriteLine($"Du tippet {isBetCorrectText}");
+
+void WriteTips()
+{
+    Console.Write("Gyldig tips: \n" +
+                  " - H, U, B\n" +
+                  " - halvgardering: HU, HB, UB\n" +
+                  " - helgardering: HUB\n" +
+                  "Hva har du tippet for denne kampen? ");
+}
+
+void WriteCommand()
+{
+    Console.Write("Kommandoer: \n" +
+                  " - H = scoring hjemmelag\n" +
+                  " - B = scoring bortelag\n" +
+                  " - X = kampen er ferdig\n" +
+                  "Angi kommando: ");
+}
