43f8ba4be3e0b2c857ef8fe2e1333cb40224ffdb
Chinzcode <132280616+Chinzcode@users.noreply.github.com>
2023-12-06 14:10:14 +0100

    feat: refactor code using Match class
    - Encapsulate functionality within the Match class
    - Simplify the main program by using Match methods

---
+++ GET Academy/Subject 2/Task Repetition/FootballBetting/FootballBetting/Match.cs
@@ -0,0 +1,38 @@
+﻿namespace FootballBetting
+{
+    internal class Match
+    {
+        private int HomeGoals;
+        private int AwayGoals;
+        private bool MatchIsRunning;
+
+        public Match()
+        {
+            this.MatchIsRunning = true;
+        }
+
+        public bool IsMatchRunning()
+        {
+            return this.MatchIsRunning;
+        }
+
+        public void CheckCommands(string command)
+        {
+            if (command == "H") this.HomeGoals++;
+            else if (command == "B") this.AwayGoals++;
+            else if (command == "X") this.MatchIsRunning = false;
+        }
+
+        public string GetCurrentScore()
+        {
+            return this.HomeGoals + "-" + this.AwayGoals;
+        }
+
+        public bool IsBetCorrect(string bet)
+        {
+            string result = this.HomeGoals == this.AwayGoals ? "U"
+                : this.HomeGoals > this.AwayGoals ? "H" : "B";
+            return bet.Contains(result);
+        }
+    }
+}
--- GET Academy/Subject 2/Task Repetition/FootballBetting/FootballBetting/Program.cs
+++ GET Academy/Subject 2/Task Repetition/FootballBetting/FootballBetting/Program.cs
@@ -1,27 +1,34 @@
-﻿Console.Write("Gyldig tips: \n" +
-              " - H, U, B\n" +
-              " - halvgardering: HU, HB, UB\n" +
-              " - helgardering: HUB\n" +
-              "Hva har du tippet for denne kampen? ");
+﻿using FootballBetting;
+
+WriteTips();
 var bet = Console.ReadLine();
-var homeGoals = 0;
-var awayGoals = 0;
-var matchIsRunning = true;
-while (matchIsRunning)
+var match = new Match();
+
+while (match.IsMatchRunning())
+{
+    WriteCommand();
+    var command = Console.ReadLine();
+    match.CheckCommands(command ?? string.Empty);
+    Console.WriteLine($"Stillingen er {match.GetCurrentScore()}.");
+}
+
+var isBetCorrectText = match.IsBetCorrect(bet ?? string.Empty) ? "riktig" : "feil";
+Console.WriteLine($"Du tippet {isBetCorrectText}");
+
+void WriteTips()
+{
+    Console.Write("Gyldig tips: \n" +
+                  " - H, U, B\n" +
+                  " - halvgardering: HU, HB, UB\n" +
+                  " - helgardering: HUB\n" +
+                  "Hva har du tippet for denne kampen? ");
+}
+
+void WriteCommand()
 {
     Console.Write("Kommandoer: \n" +
                   " - H = scoring hjemmelag\n" +
                   " - B = scoring bortelag\n" +
                   " - X = kampen er ferdig\n" +
                   "Angi kommando: ");
-    var command = Console.ReadLine();
-    matchIsRunning = command != "X";
-    if (command == "H") homeGoals++;
-    else if (command == "B") awayGoals++;
-    Console.WriteLine($"Stillingen er {homeGoals}-{awayGoals}.");
 }
-
-var result = homeGoals == awayGoals ? "U" : homeGoals > awayGoals ? "H" : "B";
-var isBetCorrect = bet.Contains(result);
-var isBetCorrectText = isBetCorrect ? "riktig" : "feil";
-Console.WriteLine($"Du tippet {isBetCorrectText}");
