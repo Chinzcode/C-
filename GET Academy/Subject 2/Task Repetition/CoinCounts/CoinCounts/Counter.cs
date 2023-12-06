4c0b02f355e4d669c434979c952a74f22cf7e221
Chinzcode <132280616+Chinzcode@users.noreply.github.com>
2023-12-06 13:24:24 +0100

    feat: create CoinCounts project with Counter class
    - Implement Counter class to represent coin values and counts
    - Calculate total sum of coin values in the main program

---
+++ GET Academy/Subject 2/Task Repetition/CoinCounts/CoinCounts/Counter.cs
@@ -0,0 +1,19 @@
+﻿namespace CoinCounts
+{
+    internal class Counter
+    {
+        private int Value;
+        private int Count;
+
+        public Counter(int value, int count)
+        {
+            this.Value = value;
+            this.Count = count;
+        }
+
+        public int CalculateValue()
+        {
+            return this.Value * this.Count;
+        }
+    }
+}
---
+++ GET Academy/Subject 2/Task Repetition/CoinCounts/CoinCounts/Program.cs
@@ -0,0 +1,18 @@
+﻿using CoinCounts;
+
+var coinCounts = new Counter[]
+{
+    new Counter(1, 7),
+    new Counter(5, 3),
+    new Counter(10, 2),
+    new Counter(10, 11),
+};
+
+int totalSum = 0;
+
+for (var i = 0; i < coinCounts.Length; i++)
+{
+    totalSum += coinCounts[i].CalculateValue();
+}
+
+Console.WriteLine("Total sum: " + totalSum);
