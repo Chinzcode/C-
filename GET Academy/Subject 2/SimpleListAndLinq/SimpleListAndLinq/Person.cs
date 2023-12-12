39a48ebbb5f7e2443fad86a55368f8f67ade4027
Chinzcode <132280616+Chinzcode@users.noreply.github.com>
2023-12-12 16:51:13 +0100

    chore: initialize project
    made new example for List and LINQ

---
+++ GET Academy/Subject 2/SimpleListAndLinq/SimpleListAndLinq/Person.cs
@@ -0,0 +1,25 @@
+﻿namespace SimpleListAndLinq
+{
+    internal class Person
+    {
+        public int Id { get; set; }
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+        public DateTime BirthDay { get; set; }
+        public int PostalCode { get; set; }
+
+        public Person(int id, string firstName, string lastName, DateTime birthDay, int postalCode)
+        {
+            Id = id;
+            FirstName = firstName;
+            LastName = lastName;
+            BirthDay = birthDay;
+            PostalCode = postalCode;
+        }
+
+        public Person()
+        {
+            
+        }
+    }
+}
---
+++ GET Academy/Subject 2/SimpleListAndLinq/SimpleListAndLinq/Program.cs
@@ -0,0 +1,26 @@
+﻿using SimpleListAndLinq;
+
+var people = new List<Person>
+{
+    new Person(
+        1, 
+        "Per", 
+        "Hansen",
+        new DateTime(1990, 1, 2),
+        1234
+        ),
+    new Person(
+        2,
+        "Pål",
+        "Nilsen",
+        new DateTime(1991, 3, 4),
+        2345
+    ),
+    new Person(
+        3,
+        "Espen",
+        "Olsen",
+        new DateTime(1992, 5, 6),
+        3456
+    )
+};
