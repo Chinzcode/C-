2cc8adf9df855c89b961ab9bf8852bbb484e3251
Chinzcode <132280616+Chinzcode@users.noreply.github.com>
2023-12-12 14:25:50 +0100

    chore: initialize project
    Example of object orientation and a bit about System.Collections.Generic.List and generics

---
+++ GET Academy/Subject 2/MyFlexibleList/MyFlexibleList/Program.cs
@@ -0,0 +1,12 @@
+﻿var numbers = new int[4];
+var count = 0;
+
+while (true)
+{
+    Console.WriteLine("Skriv et tall: ");
+    string numberStr = Console.ReadLine() ?? string.Empty;
+    int number = Convert.ToInt32(numberStr);
+    numbers[count] = number;
+    count++;
+    Console.WriteLine(string.Join(", ", numbers));
+}
