a82bc00b833ced3d9082652e330fa32802ec67d9
Chinzcode <132280616+Chinzcode@users.noreply.github.com>
2023-12-13 13:07:55 +0100

    chore: Initialize TravelAgency project and implement Agency class
    Created the TravelAgency project with an initial implementation of the Agency class.

---
+++ GET Academy/Subject 2/TravelAgency/TravelAgency/Agency.cs
@@ -0,0 +1,17 @@
+﻿namespace TravelAgency
+{
+    internal class Agency
+    {
+        private string _name;
+
+        public Agency(string name)
+        {
+            _name = name;
+        }
+
+        public void RegisterTravel(string travelName, DateTime travelDate, DateTime returnDate, int spots, int price)
+        {
+
+        }
+    }
+}
---
+++ GET Academy/Subject 2/TravelAgency/TravelAgency/Program.cs
@@ -0,0 +1,8 @@
+﻿using TravelAgency;
+
+var agency = new Agency("Tryms Reisebyrå");
+
+var travelDate = new DateTime(2024, 5, 17);
+var returnDate = new DateTime(2024, 5, 25);
+
+agency.RegisterTravel("Vinsmaking i Toscana", travelDate, returnDate, 20, 9900);
