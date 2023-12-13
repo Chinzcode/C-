812883403840c9b402415443a54881cc5dc080cf
Chinzcode <132280616+Chinzcode@users.noreply.github.com>
2023-12-13 13:54:15 +0100

    feat: Enhance TravelAgency functionality
    Added booking functionality to the Travel class.
    Introduced the Travel and Booking classes to manage travel details and bookings.
    Updated the Agency class to handle travel registration and booking retrieval.

--- GET Academy/Subject 2/TravelAgency/TravelAgency/Agency.cs
+++ GET Academy/Subject 2/TravelAgency/TravelAgency/Agency.cs
@@ -1,17 +1,39 @@
-﻿namespace TravelAgency
+﻿using System.Text;
+
+namespace TravelAgency
 {
     internal class Agency
     {
         private string _name;
+        readonly List<Travel> _travels;
 
         public Agency(string name)
         {
             _name = name;
+            _travels = new List<Travel>();
         }
 
-        public void RegisterTravel(string travelName, DateTime travelDate, DateTime returnDate, int spots, int price)
+        public Travel RegisterTravel(string name, DateTime fromDate, DateTime toDate, int capacity, int price)
         {
+            var travel = new Travel(name, fromDate, toDate, capacity, price);
+            _travels.Add(travel);
+            return travel;
+        }
 
+        public Travel GetTravel(string name)
+        {
+            return _travels.SingleOrDefault(t => t.Name == name);
+        }
+
+        public string GetAllBookingsAsText()
+        {
+            var text = new StringBuilder();
+            foreach (var travel in _travels)
+            {
+                var travelText = travel.GetAllBookingsAsText();
+                text.AppendLine(travelText);
+            }
+            return text.ToString();
         }
     }
 }
--- GET Academy/Subject 2/TravelAgency/TravelAgency/Program.cs
+++ GET Academy/Subject 2/TravelAgency/TravelAgency/Program.cs
@@ -5,4 +5,10 @@ var agency = new Agency("Tryms Reisebyrå");
 var travelDate = new DateTime(2024, 5, 17);
 var returnDate = new DateTime(2024, 5, 25);
 
-agency.RegisterTravel("Vinsmaking i Toscana", travelDate, returnDate, 20, 9900);
+var registerTravel = agency.RegisterTravel("Vinsmaking i Toscana", travelDate, returnDate, 20, 9900);
+
+var bookTravel = agency.GetTravel("Vinsmaking i Toscana");
+bookTravel.Book("Hansen", 5);
+
+var text = agency.GetAllBookingsAsText();
+Console.WriteLine(text);
