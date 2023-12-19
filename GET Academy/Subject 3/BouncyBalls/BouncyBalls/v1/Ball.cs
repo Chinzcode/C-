2ae9088f771445c4f4d5abd724b70715fcd8c9bb
Chinzcode <132280616+Chinzcode@users.noreply.github.com>
2023-12-19 16:10:57 +0100

    feat: added previous code into separate folder

---
+++ GET Academy/Subject 3/BouncyBalls/BouncyBalls/v1/Ball.cs
@@ -0,0 +1,31 @@
+﻿//namespace BouncyBalls
+//{
+//    internal class Ball
+//    {
+//        protected float _col;
+//        protected float _row;
+//        protected float _speedCol;
+//        protected float _speedRow;
+
+//        public Ball(int col, int row, int speedCol, int speedRow)
+//        {
+//            _col = col;
+//            _row = row;
+//            _speedCol = speedCol;
+//            _speedRow = speedRow;
+//        }
+
+//        public virtual void Show(bool changeColor = true)
+//        {
+//            Console.CursorLeft = Convert.ToInt32(_col);
+//            Console.CursorTop = Convert.ToInt32(_row);
+//            Console.Write("O");
+//        }
+
+//        public virtual void Move()
+//        {
+//            _col += _speedCol;
+//            _row += _speedRow;
+//        }
+//    }
+//}
